52090848f2f08f6fb2279a0d0a0d6b751c2863ab
MASHRAFI47 <mbngms@gmail.com>
2024-05-13 05:30:14 +0600

    cors and json file type added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+require('dotenv').config();
+
+const cors = require("cors");
+app.use(cors())
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+
 app.get('/', (req, res) => {
     res.send("Crimson Suite Server is Running")
 })
