0a18acf591feb11a41711af9f5438f659b1a6034
MASHRAFI47 <mbngms@gmail.com>
2024-05-14 17:08:19 +0600

    added vercel rooms

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 
 //cors
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://crimson-suite.web.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://crimson-suite.web.app/', 'https://crimson-suite-server.vercel.app/rooms'],
     credentials: true,
     optionSuccessStatus: true
 }
