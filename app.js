896b499844c08a367f55c854e580e85934d63939
MASHRAFI47 <mbngms@gmail.com>
2024-05-14 17:31:46 +0600

    minor fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 
 //cors
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://crimson-suite-server.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://crimson-suite-server.vercel.app'],
     credentials: true,
     optionSuccessStatus: true
 }
